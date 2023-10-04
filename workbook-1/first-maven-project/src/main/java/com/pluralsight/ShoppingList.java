ada69776cc7e3551dc3ad8d44d567748ff02e9b2
Tunkz <Bubatunkz00@gmail.com>
2023-10-04 09:44:25 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+
+        System.out.println("Ketchup");
+        System.out.println("Mayo");
+        System.out.println("Butter");
+        System.out.println("Peanutbutter");
+        System.out.println("Strawberry");
+        System.out.println("Windex");
+        System.out.println("Watermelon");
+        System.out.println("Groundnut");
+        System.out.println("Chicken");
+        System.out.println("Meat");
+    }
+}
